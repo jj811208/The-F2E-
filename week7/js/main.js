ac02a2f90996eeec82de391fc984f1019d74125b
jj811208 <jj811208@gmail.com>
2018-07-20 00:02:21 +0800

    ok
    ok

--- week7/js/main.js
+++ week7/js/main.js
@@ -33,6 +33,72 @@ ctx.fillText("Radio Defense",gamewidth/2,gameheight/2+10);
 ctx.font="18px Microsoft JhengHei";
 ctx.fillText("Start Game",gamewidth/2,gameheight/2+90);
 
+ctx.beginPath();
+  ctx.strokeStyle = '#fff';
+  ctx.moveTo(gamewidth/2-50,gameheight/2+69);
+  ctx.lineTo(gamewidth/2+48,gameheight/2+69);
+  ctx.arc(gamewidth/2+48,gameheight/2+84,15,1.5*Math.PI,2.5*Math.PI);
+  ctx.lineTo(gamewidth/2-50,gameheight/2+99);
+  ctx.arc(gamewidth/2-50,gameheight/2+84,15,0.5*Math.PI,1.5*Math.PI);
+  ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+  ctx.strokeStyle = enemyYellow;
+  ctx.fillStyle = enemyYellow;
+  ctx.rect(gamewidth/2-65,gameheight/2-100,40,60);
+  ctx.rect(gamewidth/2-65,gameheight/2-35,40,5);
+  ctx.fill();
+  ctx.stroke();
+  
+ctx.beginPath();
+  ctx.strokeStyle = '#fff';
+  ctx.fillStyle = '#fff';
+  ctx.rect(gamewidth/2-55,gameheight/2-107,20,7);
+  ctx.fill();
+  ctx.stroke();
+  
+ctx.beginPath();
+  ctx.strokeStyle = '#fff';
+  ctx.fillStyle = '#fff';
+  ctx.moveTo(gamewidth/2-45,gameheight/2-90);
+  ctx.lineTo(gamewidth/2-55,gameheight/2-68);
+  ctx.lineTo(gamewidth/2-43,gameheight/2-68);
+  ctx.lineTo(gamewidth/2-45,gameheight/2-50);
+  ctx.lineTo(gamewidth/2-35,gameheight/2-72);
+  ctx.lineTo(gamewidth/2-47,gameheight/2-72);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  
+  
+ctx.beginPath();
+  ctx.strokeStyle = '#fff';
+  ctx.fillStyle = '#fff';
+  ctx.moveTo(gamewidth/2,gameheight/2-30);
+  ctx.lineTo(gamewidth/2,gameheight/2-140);
+  ctx.lineTo(gamewidth/2+55,gameheight/2-140);
+  ctx.arc(gamewidth/2+55,gameheight/2-110,30,1.5*Math.PI,2.4*Math.PI);
+  ctx.bezierCurveTo(gamewidth/2+90, gameheight/2-80, gamewidth/2+75, gameheight/2-30, gamewidth/2+90, gameheight/2-30);
+  ctx.lineTo(gamewidth/2+70,gameheight/2-30);
+  ctx.bezierCurveTo(gamewidth/2+65, gameheight/2-35, gamewidth/2+65, gameheight/2-40, gamewidth/2+65, gameheight/2-60);
+  ctx.bezierCurveTo(gamewidth/2+50, gameheight/2-57, gamewidth/2+50, gameheight/2-57, gamewidth/2+55, gameheight/2-73);
+  ctx.lineTo(gamewidth/2+20, gameheight/2-73);
+  ctx.lineTo(gamewidth/2+20, gameheight/2-30);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  
+ctx.beginPath();
+  ctx.strokeStyle = backgroundColor;
+  ctx.fillStyle = backgroundColor;
+  ctx.moveTo(gamewidth/2+20,gameheight/2-90);
+  ctx.lineTo(gamewidth/2+20,gameheight/2-126);
+  ctx.lineTo(gamewidth/2+50,gameheight/2-126);
+  ctx.arc(gamewidth/2+50,gameheight/2-108,18,1.5*Math.PI,2.5*Math.PI);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
 
 
 
@@ -80,5 +146,3 @@ ctx.textAlign="left";
 ctx.fillText("你身負著運送能量電池的任務",10,gameheight-80);
 ctx.fillText("卻遭到幾何星人的埋伏",10,gameheight-50);
 ctx.fillText("請協助從它們的手中奪回能量電池",10,gameheight-20);
-
-ctx.font="40px Georgia";
